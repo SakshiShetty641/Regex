69cfd6e78469bc9c3dc518c004def01eb4f5dcf2
Sakshi <sakshishetty1231997@gmail.com>
2021-08-25 18:31:40 +0530

    Added: To validate the entered first name

--- src/com/bridgelabz/Regex.java
+++ src/com/bridgelabz/Regex.java
@@ -1,12 +1,17 @@
 package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 /**
- * Purpose - To implement usage of Java Regular Expressions and Patterns
+ * Purpose - To validate the first name using regex
  * @author - Sakshi Shetty
  * @version - 16.0
  * @since - 2021-08-25
  */
-public class Regex {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Program");
+
+    public class Regex {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Program");
+            UserRegistration userRegistration = new UserRegistration();
+            userRegistration.validateUserDetails();
+        }
     }
-}
---
+++ src/com/bridgelabz/UserDetails.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+/**
+ * Purpose - Implementing getters and setters
+ * @Author - Sakshi Shetty
+ */
+public class UserDetails {
+    String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
