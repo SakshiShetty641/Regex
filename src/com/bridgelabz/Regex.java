88fc3d8604b85f07df55ae3008a04467ac686817
Sakshi <sakshishetty1231997@gmail.com>
2021-08-25 16:00:23 +0530

    Added: Welcome Message to the program

---
+++ src/com/bridgelabz/Regex.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement usage of Java Regular Expressions and Patterns
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-08-25
+ */
+public class Regex {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Program");
+    }
+}
